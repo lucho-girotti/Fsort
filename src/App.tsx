d2f677eb2fd54bcd02fe8ad25ee6e4084371965c
lucho-girotti <onaicul2008@gmail.com>
2024-12-02 20:34:00 -0300

    Setup Complete

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from './components/Title';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Title />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Title.tsx
@@ -0,0 +1,8 @@
+// src/components/HelloWorld.tsx
+import React from 'react';
+
+const Title: React.FC = () => {
+  return <h1>Fsort</h1>;
+};
+
+export default Title;
