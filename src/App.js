75d2fed69f0ef51b720e06ef435037e249636316
lucho-girotti <onaicul2008@gmail.com>
2024-12-02 20:45:38 -0300

    fix

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import Title from './components/Title';
+const App = () => {
+    return (_jsx("div", { children: _jsx(Title, {}) }));
+};
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+const Title = () => {
+    return _jsx("h1", { children: "Fsort" });
+};
+export default Title;
